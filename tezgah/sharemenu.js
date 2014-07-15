dffa6765bdc4fdb2e5241aab38a7a279766e8ae4
hozakar <hozakar@gmail.com>
2014-07-15 18:59:11 +0300

    StartUp

--- tezgah/sharemenu.js
+++ tezgah/sharemenu.js
@@ -1,8 +1,10 @@
 (function($) {
 	"use strict";
 	
+	var allmenus = new Array();
+	
 	var sharemenu = {
-		selisleri: function(e) {
+		selisleri: function(e, el) {
 			var hoy = getSelection().toString();
 			while(hoy.split('\n').length > 1) hoy = hoy.split('\n').join(' ');
 			while(hoy.split('  ').length > 1) hoy = hoy.split('  ').join(' ');
@@ -10,10 +12,10 @@
 			while(hoy.substr(0, 1) === ' ') hoy = hoy.substr(-1 * (hoy.length - 1));
 			while(hoy.substr((hoy.length - 1), 1) === ' ') hoy = hoy.substr(0, hoy.length - 1);
 			
-			this.openShareMenu(hoy, e);
+			this.openShareMenu(hoy, e, el);
 		},
-		openShareMenu: function(hoy, e) {
-			var $menu = this.find("._sharemenu_menu_list_");
+		openShareMenu: function(hoy, e, el) {
+			var $menu = $(el).find("._sharemenu_menu_list_");
 			if($menu.css('display') != 'none') {
 				if(!hoy) {
 					$menu.data('sel', hoy);
@@ -34,6 +36,7 @@
 			
 			$menu.html(
 				'<li><a href="' + twit + '" target="_blank"><i class="fa fa-twitter"></i>&nbsp;Tweet (' + twittext.length + ' Karakter)</a></li>' + 
+				'<li><a href="" target="_blank"><i class="fa fa-facebook"></i>&nbsp;Facebook</a></li>' + 
 				'<li><a href="#" target="_self"><i class="fa fa-search"></i>&nbsp;Sitede Ara</a></li>' + 
 				'<li><a href="' + goog + '" target="_blank"><i class="fa fa-google"></i>&nbsp;Google\'da Ara</a></li>'
 			);
@@ -41,16 +44,39 @@
 		},
 		init: function(el, param) {
 			el.append('<ul class="_sharemenu_menu_list_"></ul>');
-
+			
 			el.mouseup(function(e){
+				var sm = allmenus[$(this).data('sharemenuid')];
 				e = e || window.event;
 				if(e.button !== 0) return;
 				e.preventDefault();
-				this.selisleri(e);
+				sm.selisleri(e, this);
 			});
 			
 		}
 	}
+	/* Plug-in Start */
+	$.fn.sharemenu = function(param) {
+		allmenus.push(new Object());
+		var id = allmenus.length-1;
+		allmenus[id] = sharemenu;
+		allmenus[id].id = id;
+		this.data('sharemenuid', id);
+		
+		allmenus[id].init(this, param);
+	}
+	/* End: Plug-in Start */
+	
+	$(function(){
+		$('body').mousedown(function(e){
+			e = e || window.event;
+			if(e.button === 0) return;
+			$("._sharemenu_menu_list_").css('display', 'none');
+		});
+	});
+})(jQuery);
+
+	/*
 	function selisleri(e) {
 		var hoy = getSelection().toString();
 		while(hoy.split('\n').length > 1) hoy = hoy.split('\n').join(' ');
@@ -61,25 +87,6 @@
 		
 		openShareMenu(hoy, e);
 	}
-
-	$(function(){
-		/*
-		$('body').append('<ul id="_sharemenu_menu_list_"></ul>');
-		*/
-		$('body').mousedown(function(e){
-			e = e || window.event;
-			if(e.button === 0) return;
-			$("._sharemenu_menu_list_").css('display', 'none');
-		});
-		/*
-		$('body').mouseup(function(e){
-			e = e || window.event;
-			if(e.button !== 0) return;
-			e.preventDefault();
-			selisleri(e);
-		});
-		*/
-	});
 
 	function openShareMenu(hoy, e) {
 		var $menu = $("#_sharemenu_menu_list_");
@@ -108,11 +115,4 @@
 		);
 		$menu.css('display', 'block');
 	}
-	
-	/* Plug-in Start */
-	$.fn.sharemenu = function(param) {
-		sharemenu.init(this, param);
-	}
-	/* End: Plug-in Start */
-	
-})(jQuery);
+	*/
